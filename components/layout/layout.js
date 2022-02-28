6aaceaf846ad6aca2485cdc5a62c6415c0d9fbdc
YeyoM <yeyomoreno2003@hotmail.com>
2022-02-27 20:42:23 -0600

    nav

---
+++ components/layout/layout.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react'
+
+import MainHeader from './main-header'
+
+export default function Layout(props) {
+
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>
+        {props.children}
+      </main>
+    </Fragment>
+  )
+
+}
